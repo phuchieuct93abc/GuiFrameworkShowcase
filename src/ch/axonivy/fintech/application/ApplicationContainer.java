0f754c6886889a0fe40e2589f00a53d539514a7a
phuchieu <phuchieuct93@gmail.com>
2017-08-10 09:05:08 +0700

    Update

--- src/ch/axonivy/fintech/application/ApplicationContainer.java
+++ src/ch/axonivy/fintech/application/ApplicationContainer.java
@@ -1,8 +1,13 @@
 package ch.axonivy.fintech.application;
 
+import java.io.IOException;
+
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 
+import ch.ivyteam.ivy.environment.EnvironmentNotAvailableException;
 import ch.ivyteam.ivy.environment.Ivy;
 
 @ManagedBean(eager = true)
@@ -15,21 +20,20 @@ public class ApplicationContainer {
 		return target.equals(activeLink);
 	}
 
-	
 	public ShowCaseEnum getActiveLink() {
 		return activeLink;
 	}
-
 
 	public void setActiveLink(ShowCaseEnum activeLink) {
 		this.activeLink = activeLink;
 	}
 
-
-	public String goShowcase(ShowCaseEnum target) {
+	public void goShowcase(ShowCaseEnum target) throws EnvironmentNotAvailableException, IOException {
 		this.activeLink = target;
+		Ivy.log().debug(target);
+		ExternalContext ex = FacesContext.getCurrentInstance().getExternalContext();
+		ex.redirect(Ivy.html().startref(target.getTarget()));
 
-		return Ivy.html().startref(target.getTarget());
 
 	}
 }
