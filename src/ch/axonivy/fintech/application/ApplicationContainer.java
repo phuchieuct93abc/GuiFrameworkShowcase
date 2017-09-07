dae5366f6634d31fd4a50b729e99450e966f8564
phuchieu <phuchieuct93@gmail.com>
2017-09-07 18:33:06 +0700

    Update

--- src/ch/axonivy/fintech/application/ApplicationContainer.java
+++ src/ch/axonivy/fintech/application/ApplicationContainer.java
@@ -1,39 +1,90 @@
 package ch.axonivy.fintech.application;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
-import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 
-import ch.ivyteam.ivy.environment.EnvironmentNotAvailableException;
+import org.primefaces.model.menu.DefaultMenuItem;
+import org.primefaces.model.menu.DefaultMenuModel;
+import org.primefaces.model.menu.DefaultSubMenu;
+import org.primefaces.model.menu.MenuElement;
+import org.primefaces.model.menu.MenuItem;
+import org.primefaces.model.menu.MenuModel;
+import org.primefaces.model.menu.Submenu;
+
 import ch.ivyteam.ivy.environment.Ivy;
 
+import com.google.inject.servlet.SessionScoped;
+
 @ManagedBean(eager = true)
-@ApplicationScoped
+@SessionScoped
 public class ApplicationContainer {
-	private ShowCaseEnum activeLink;
+	private ShowCaseEnum activeShowcase;
+
+	public MenuModel getMenu() {
+		MenuModel menu = new DefaultMenuModel();
+
+		// First submenu
+		DefaultSubMenu componentMenus = getComponentSubMenu();
+		DefaultSubMenu useCaseMenus = getUsecaseSubMenu();
+
+		menu.addElement(componentMenus);
+		menu.addElement(useCaseMenus);
+
+		return menu;
+	}
+
+	private DefaultSubMenu getComponentSubMenu() {
+		DefaultSubMenu componentSubMenu = new DefaultSubMenu("Component");
+		List<MenuElement> elements = ShowCaseEnum.getBasicCompomnentMenus()
+				.stream().map(this::generateMenuItem)
+				.collect(Collectors.toList());
+		componentSubMenu.setElements(elements);
+		return componentSubMenu;
+
+	}
+
+	private DefaultSubMenu getUsecaseSubMenu() {
+		DefaultSubMenu componentSubMenu = new DefaultSubMenu("Use cases");
+		List<MenuElement> elements = ShowCaseEnum.getBasicCompomnentMenus()
+				.stream().map(this::generateMenuItem)
+				.collect(Collectors.toList());
+		componentSubMenu.setElements(elements);
+		return componentSubMenu;
+
+	}
+
+	private DefaultMenuItem generateMenuItem(ShowCaseEnum menu) {
+		String activeClass = menu == activeShowcase ? "ui-active-state" : "";
+		DefaultMenuItem item = new DefaultMenuItem(menu.getName());
+		item.setOnstart("setActiveMenu(" + menu + ")");
+		item.setStyleClass(activeClass);
+		return item;
+
+	}
 
 	public Boolean isActive(ShowCaseEnum target) {
 
-		return target.equals(activeLink);
+		return target.equals(activeShowcase);
 	}
 
 	public ShowCaseEnum getActiveLink() {
-		return activeLink;
+		return activeShowcase;
 	}
 
 	public void setActiveLink(ShowCaseEnum activeLink) {
-		this.activeLink = activeLink;
+		this.activeShowcase = activeLink;
 	}
 
-	public void goShowcase(ShowCaseEnum target) throws EnvironmentNotAvailableException, IOException {
-		this.activeLink = target;
-		Ivy.log().debug(target);
-		ExternalContext ex = FacesContext.getCurrentInstance().getExternalContext();
-		ex.redirect(Ivy.html().startref(target.getTarget()));
-
+	public void goShowcase(ShowCaseEnum target) throws IOException {
+		this.activeShowcase = target;
+		ExternalContext ex = FacesContext.getCurrentInstance()
+				.getExternalContext();
+		ex.redirect(Ivy.html().startref(target.getLink()));
 
 	}
 }
--- src/ch/axonivy/fintech/application/ShowCaseEnum.java
+++ src/ch/axonivy/fintech/application/ShowCaseEnum.java
@@ -1,21 +1,43 @@
 package ch.axonivy.fintech.application;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum ShowCaseEnum {
 
-	GETTING_START ("ShowCaseProcesses/GettingStart/start.ivp"),
-	BASIC_COMPONENT ( "ShowCaseProcesses/BasicComponent/start.ivp"),
-	AUTO_COMPLETE ( "ShowCaseProcesses/BasicComponent/start.ivp"),
-	VALIDATE_BUTTON ( "UseCaseProcesses/ValidateButton/start.ivp"),
-	CUSTOM_CONTROLLER( "ShowCaseProcesses/CustomController/start.ivp"),
-	COMPONENT_CONTEXT( "UseCaseProcesses/ComponentContext/start.ivp");
+	GETTING_START ("ShowCaseProcesses/GettingStart/start.ivp","Getting startted"),
+	BASIC_COMPONENT ( "ShowCaseProcesses/BasicComponent/start.ivp","Basic Component"),
+	AUTO_COMPLETE ( "ShowCaseProcesses/BasicComponent/start.ivp","Auto complete"),
+	VALIDATE_BUTTON ( "UseCaseProcesses/ValidateButton/start.ivp","Validate button"),
+	CUSTOM_CONTROLLER( "ShowCaseProcesses/CustomController/start.ivp","Custom controller"),
+	COMPONENT_CONTEXT( "UseCaseProcesses/ComponentContext/start.ivp","Component context");
 	 
-	private String target;
 
-	public String getTarget() {
-		return target;
+
+	private String link;
+	private String name;
+
+
+	 ShowCaseEnum(String link,String name) {
+		this.link = link;
+		this.name = name;
+	}
+	public String getLink() {
+		return link;
 	}
 
-	private ShowCaseEnum(String target) {
-		this.target = target;
+	public String getName() {
+		return name;
+	}
+	public static List<ShowCaseEnum> getBasicCompomnentMenus(){
+		return Arrays.asList(BASIC_COMPONENT,AUTO_COMPLETE,VALIDATE_BUTTON);
+		
+		
+	}
+	public static List<ShowCaseEnum> getUsecaseMenus(){
+		
+		return Arrays.asList(CUSTOM_CONTROLLER,AUTO_COMPLETE,COMPONENT_CONTEXT);
+
+		
 	}
 }
